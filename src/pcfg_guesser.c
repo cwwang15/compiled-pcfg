69ebd89771b1911ce16c52aa148fbdcbe3ccab64
Chuanwang WANG <chaunecy@outlook.com>
2021-05-10 19:00:30 +0800

    docs(done): print msg when completing generation

--- src/pcfg_guesser.c
+++ src/pcfg_guesser.c
@@ -182,6 +182,7 @@ int main(int argc, char *argv[]) {
         gettimeofday(&end, NULL);
         double timeuse = (double) (end.tv_sec - start.tv_sec) + ((double) (end.tv_usec - start.tv_usec) / 1000000);
         if (cur_gen_num > guess_number || timeuse > 2.0) {
+            fprintf(stderr, "We generated %lld guesses!", cur_gen_num);
             exit(0);
         }
         free(pq_item->pt);
